e77587d57843dc0d1ca52a3e3c4cf57971de7fce
najaf2 <najafali@ou.edu>
2021-07-14 01:09:29 -0500

    hovering changes div color

--- script.js
+++ script.js
@@ -14,3 +14,35 @@ for (let i = 0; i < size; ++i) {
     div.id = "grids"
     outerDiv.appendChild(div);
 }
+
+
+let elementsArray = document.querySelectorAll("#grids");
+
+elementsArray.forEach(function(elem) {
+    elem.addEventListener("mouseenter", function() {
+        this.style.backgroundColor = randomColor();
+    });
+});
+
+function randomColor() {
+    const rndInt = Math.floor(Math.random() * 7) + 1
+
+    switch (rndInt) {
+        case 1:
+            return "red";
+        case 2:
+            return "orange";
+        case 3:
+            return "yellow";
+        case 4:
+            return "green";
+        case 5:
+            return "blue";
+        case 6:
+            return "indigo";
+        case 7:
+            return "violet";
+    }
+}
+
+
