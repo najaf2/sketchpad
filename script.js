89fd9d56538227feda47bc4f70ebd6cf924fc5ca
najaf2 <najafali@ou.edu>
2021-07-16 01:18:43 -0500

    Rework logic, add borders

--- script.js
+++ script.js
@@ -1,55 +1,73 @@
-const outerDiv = document.querySelector('.outer');
+const canvas = document.querySelector('.outer');
 const resetBtn = document.querySelector('#resetBtn')
+const blackBtn = document.querySelector('#blackPen');
+const rainBowBtn = document.querySelector('#rainbowPen');
+const colorPicker = document.querySelector('#colorpicker');
+const slider = document.querySelector('#slider');
+let on = true;
 
-let size = 16*16;
+let size = 50*50;
+let penColor = "black";
 
-for (let i = 0; i < size; ++i) {
-    let hw = 720/Math.sqrt(size);
+let elementsArray;
+makeDivs();
+addHover();
 
-    let div = document.createElement("div");
-    div.style.backgroundColor = "white";
-    div.style.width = hw.toString() + "px";
-    div.style.height = hw.toString() + "px";
-    div.style.float = "left";
-    div.id = "grids"
-    outerDiv.appendChild(div);
+
+
+
+
+// make divs
+function makeDivs() {
+    for (let i = 0; i < size; ++i) {
+        let hw = (720 - Math.sqrt(size)*2)/Math.sqrt(size);
+
+        let div = document.createElement("div");
+        div.style.backgroundColor = "white";
+        div.style.width = hw.toString() + "px";
+        div.style.height = hw.toString() + "px";
+        div.style.float = "left";
+        div.id = "grids"
+        canvas.appendChild(div);
+    }
+    elementsArray = document.querySelectorAll("#grids");
+
 }
 
 
-let elementsArray = document.querySelectorAll("#grids");
 
+//add mouse hover over canvas effect
+function addHover() {
+    elementsArray.forEach(function(elem) {
+        elem.addEventListener("mouseenter", function(elem) {
+            this.style.backgroundColor = penColor;
+
+        })
+    });
+}
+
+// Reset button clicked.
 resetBtn.addEventListener("click", () => {
     for (let i = 0; i < elementsArray.length; ++i) {
         elementsArray[i].style.backgroundColor = "white";
     }});
 
-elementsArray.forEach(function(elem) {
-    elem.addEventListener("mouseenter", function() {
-        this.style.backgroundColor = randomColor();
-    });
+// Change color
+colorPicker.addEventListener("input", function(elem) {
+    penColor = elem.target.value.toString();
+})
+
+// Change size of canvas
+slider.addEventListener("input", function() {
+    size = this.value * this.value;
+    removeAllChildNodes(canvas);
+    makeDivs();
+    addHover();
+
 });
 
-
-
-function randomColor() {
-    const rndInt = Math.floor(Math.random() * 7) + 1
-
-    switch (rndInt) {
-        case 1:
-            return "red";
-        case 2:
-            return "orange";
-        case 3:
-            return "yellow";
-        case 4:
-            return "green";
-        case 5:
-            return "blue";
-        case 6:
-            return "indigo";
-        case 7:
-            return "violet";
+function removeAllChildNodes(parent){
+    while(parent.firstChild){
+        parent.removeChild(parent.firstChild);
     }
 }
-
-
