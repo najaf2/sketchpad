2117de8fafe53526d30b626db6a9c344449ed26d
najaf2 <najafali@ou.edu>
2021-07-18 21:53:51 -0500

    Rework hover and mousedown logic, make all buttons functional

--- script.js
+++ script.js
@@ -4,14 +4,20 @@ const blackBtn = document.querySelector('#blackPen');
 const rainBowBtn = document.querySelector('#rainbowPen');
 const colorPicker = document.querySelector('#colorpicker');
 const slider = document.querySelector('#slider');
-let on = true;
 
-let size = 50*50;
+const all = document.querySelector("#all");
+
+let size = 25*25;
 let penColor = "black";
+let rainbowClicked = false;
 
 let elementsArray;
+let mouseDown = false;
 makeDivs();
 addHover();
+addDown();
+addUp();
+addClick();
 
 // make divs
 function makeDivs() {
@@ -30,15 +36,54 @@ function makeDivs() {
 
 }
 
+function setDown() {
+    mouseDown = true;
+}
+function setUp() {
+    mouseDown = false;
+}
 
 
 //add mouse hover over canvas effect
+function addDown() {
+    elementsArray.forEach(function(elem) {
+        elem.addEventListener("mousedown", function(elem) {
+            mouseDown = true;
+        })
+    });
+}
+
+function addUp() {
+    elementsArray.forEach(function(elem) {
+        elem.addEventListener("mouseup", function(elem) {
+            mouseDown = false;
+        })
+    });
+}
+
+
+
 function addHover() {
     elementsArray.forEach(function(elem) {
         elem.addEventListener("mouseenter", function(elem) {
-            this.style.backgroundColor = penColor;
-
+            if (mouseDown == true) {
+                if (rainbowClicked)
+                    this.style.backgroundColor = random();
+                else
+                    this.style.backgroundColor = penColor;
+            }
         })
+    });
+}
+
+function addClick() {
+    elementsArray.forEach(function(elem) {
+        elem.addEventListener("click", function(elem) {
+        if (rainbowClicked)
+            this.style.backgroundColor = random();
+        else
+            this.style.backgroundColor = penColor;       
+         })
     });
 }
 
@@ -46,24 +91,86 @@ function addHover() {
 resetBtn.addEventListener("click", () => {
     for (let i = 0; i < elementsArray.length; ++i) {
         elementsArray[i].style.backgroundColor = "white";
-    }});
+    }
+});
 
 // Change color
 colorPicker.addEventListener("input", function(elem) {
+    rainbowClicked = false;
     penColor = elem.target.value.toString();
 })
 
 // Change size of canvas
 slider.addEventListener("input", function() {
     size = this.value * this.value;
-    removeAllChildNodes(canvas);
+    removeAllDivs(canvas);
     makeDivs();
     addHover();
+    addDown();
+    addUp();
+    addClick();
+
 
 });
 
-function removeAllChildNodes(parent){
+rainBowBtn.addEventListener("click", function() {
+    if (rainbowClicked) {
+        rainbowClicked = false;
+    }
+    else {
+        rainbowClicked = true;
+    }
+});
+
+blackBtn.addEventListener("click", function() {
+    rainbowClicked = false;
+    penColor = "black";
+})
+
+
+function removeAllDivs(parent){
     while(parent.firstChild){
         parent.removeChild(parent.firstChild);
     }
 }
+
+function random() {
+    let num = Math.floor((Math.random() * 14) + 1);
+
+    switch (num) {
+        case 1:
+            return "red";
+        case 2:
+            return "orange";
+        case 3:
+            return "green";
+        case 4:
+            return "blue";
+        case 5:
+            return "indigo";
+        case 6:
+            return "violet";
+        case 7:
+            return "yellow";
+        case 8:
+            return "purple";
+        case 9:
+            return "pink";
+        case 10:
+            return "skyblue";
+        case 11:
+            return "gold";
+        case 12:
+            return "beige";
+        case 13:
+            return "brown";
+        case 14:
+            return "black";
+
+    }
+}
+
+
+
+
+
